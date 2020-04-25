15b0b3dce675b9f8949d6bb9c3acf127537cb36d
Phonekham <phone.madridista@gmail.com>
2020-04-25 16:12:15 +0700

    schema

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const { ApolloServer, gql } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schema");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+const PORT = 4444;
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () =>
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  )
+);
